61cafc36365e3bec563a2c526deef5356e12bec1
Al Scott <scottaj2@udmercy.edu>
2012-09-18 03:40:38 -0400

    Removed database cleaner config from cucumber env file.
    Since we had no database set up yet, the inclusion of this config prematurely was causing cucumber to hang indefinately.

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,10 +4,6 @@ require File.expand_path(File.dirname(__FILE__) + "/../../config/boot")
 require 'capybara/cucumber'
 require 'rspec/expectations'
 require 'prickle/capybara'
-require 'database_cleaner'
-require 'database_cleaner/cucumber'
-
-DatabaseCleaner.strategy = :transaction
 
 World do
   include Capybara::DSL
@@ -18,13 +14,3 @@ end
 # You can handle all padrino applications using instead:
 #   Padrino.application
 Capybara.app = CourseOutcomes.tap { |app|  }
-
-
-Before do
-  DatabaseCleaner.clean_with :truncation
-  DatabaseCleaner.start
-end
-
-After do
-  DatabaseCleaner.clean
-end
