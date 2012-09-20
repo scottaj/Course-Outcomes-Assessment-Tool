c7a0ed292f9ad8338326ea9041eace4c49ea0a31
Glenn Roy Bishop Jr <sifelaver@gmail.com>
2012-09-19 17:48:16 -0700

    Added framework for homepage layout, fixed typo
    May or may not need homepage layout, possibly add some interesting
    things.
    Added a demo login to login.rb. No user yet.

---
+++ app/controllers/homepage.rb
@@ -0,0 +1,5 @@
+CourseOutcomes.controllers :homepage do
+  get :index do
+    render "home/home"
+  end
+end
--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -2,4 +2,16 @@ CourseOutcomes.controllers :login do
   get :index do
     render "login/login"
   end
+  #def new
+   # @user_session = UserSession.new
+  #end
+ # def create
+  #  @user_session = UserSession.new(params[:user_session])
+  #  if @user_session.save
+  #    flash[:notice] = "Login successful!"
+  #    redirect_back_or_default user_path
+  #  else
+  #    render :action => :new
+  #  end
+  #end
 end
