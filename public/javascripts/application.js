65643cf3c50ca57ef3eb5c0c8ff7b0f0e62bd96e
Glenn Roy Bishop Jr <sifelaver@gmail.com>
2012-10-17 22:52:58 -0700

    Added functionality for reactivating users who have been previously deactivated. Buttons that control the showing and hiding now work, but the text on the buttons does not.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,9 +8,26 @@ function show_archived_courses() {
           $("#center-box").append(data);
         });
   $("#archive-button").attr('onclick', '').click(hide_archived_courses);
-  $("#archive-button").text("Hide Archived");
+};
+
+function show_deactivated_users() {
+  $.get('/admin/admin/user/deactivated', {}, function(data) {
+          data = "<div id=\"deactivated-users\">" + 
+            "<div class=\"pushdown\"></div>" +
+            "<h1>Deactivated Users</h1>" +
+            data +
+            "</div>";
+          $("#center-box").append(data);
+        });
+  $("#reactivate-button").attr('onclick', '').click(hide_deactivated_users);
+  $("#reactivate-button").text("Hide Deactivated");  
 };
 
 function hide_archived_courses() {
-  
+$("#archived-courses").toggle();
+
+};
+
+function hide_deactivated_users() {
+$("#deactivated-users").toggle();
 };
