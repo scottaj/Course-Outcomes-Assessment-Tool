b29937aaa837fa5440b4a29758429de791d7ce65
Al Scott <scottaj2@udmercy.edu>
2012-10-19 01:16:59 -0400

    Staging changes to js file to resolve lost code conflict.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,5 @@
 function show_archived_courses() {
+  // Get new data and add it to the bottom of the page
   $.get('/course/archived', {}, function(data) {
           data = "<div id=\"archived-courses\">" +
             "<div class=\"pushdown\"></div>" +
@@ -7,12 +8,17 @@ function show_archived_courses() {
             "</div>";
           $("#center-box").append(data);
         });
+  // Change button text and bind it to new function
   $("#archive-button").attr('onclick', '').click(hide_archived_courses);
   $("#archive-button").text("Hide Archived");
 };
 
+
 function hide_archived_courses() {
+  // Remove archived course listing.
   $("#archived-courses").remove();
+
+  // Change button text and bind it to new function
   $("#archive-button").click(show_archived_courses);
   $("#archive-button").text("Show Archived");
 };
