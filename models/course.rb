4fe998fa5697d8b1bd053778e5d484b6c3c6fa6f
Al Scott <scottaj2@udmercy.edu>
2012-10-19 01:11:49 -0400

    Added outcome model specs.

--- models/course.rb
+++ models/course.rb
@@ -1,6 +1,8 @@
 class Course < ActiveRecord::Base
   belongs_to :professor, class_name: "User", foreign_key: "user_id", inverse_of: :courses
 
+  has_many :outcomes, dependent: :nullify, inverse_of: :course
+  
   validates_presence_of :course_name, :course_title, :term_number, :term_year, :professor, message: "Please enter all required fields"
 
   validates_uniqueness_of :course_title, scope: [:term_year, :term_number, :section]
@@ -8,3 +10,5 @@ class Course < ActiveRecord::Base
     return "#{self.course_title.gsub(/\s+/, "").downcase}-#{self.section}-#{self.term_number}-#{self.term_year}"
   end
 end
+
+
--- spec/models/outcome_spec.rb
+++ spec/models/outcome_spec.rb
@@ -1,8 +1,50 @@
 require 'spec_helper'
 
 describe "Outcome Model" do
-  let(:outcome) { Outcome.new }
+  before do
+    @p = User.create(username: "norrisc",
+                    first_name: "Chuck",
+                    last_name: "Norris",
+                    password: "abcd1234")
+    @c = Course.create(course_name: "Roundhouse Kicking",
+                      course_title: "AK 101",
+                      term_number: 1,
+                      term_year: 2012,
+                      professor: User.find_by_username("norrisc"))
+  end
+
   it 'can be created' do
+    outcome = Outcome.new
     outcome.should_not be_nil
+  end
+
+  describe "validations" do
+    describe "enum" do
+      it "should be unique between two outcomes with the same course id" do
+        o = Outcome.create(enum: "a",                     
+                           course: @c)
+
+        o2 = Outcome.new(enum: "a",
+                         course: @c)
+
+        o2.should_not be_valid
+      end
+
+      it "can be the same if two outcomes have different course id's" do
+        o = Outcome.create(enum: "a",                     
+                           course: @c)
+        o2 = Outcome.new(enum: "a",
+                         course_id: 999)
+
+        o2.should be_valid
+      end
+
+      it "should be an auto-generated incrementing string if not manually specified" do
+        x = []
+        3.times {x << Outcome.create(course: @c)}
+
+        x.map {|i| i.enum}.should =~ ["a", "b", "c"]
+      end
+    end
   end
 end
