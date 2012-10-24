647da4e2028bc0e7aaf70468b8b39df8b5011727
Al Scott <scottaj2@udmercy.edu>
2012-10-24 14:04:23 -0400

    Fixed Migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 5) do
-
+ActiveRecord::Schema.define(:version => 6) do
 
   create_table "courses", :force => true do |t|
     t.string   "course_name"
@@ -27,16 +25,17 @@ ActiveRecord::Schema.define(:version => 5) do
     t.datetime "updated_at",                      :null => false
   end
 
+  create_table "courses_students", :id => false, :force => true do |t|
+    t.integer "student_id", :null => false
+    t.integer "course_id",  :null => false
+  end
 
   create_table "outcomes", :force => true do |t|
     t.string   "enum"
     t.text     "outcome"
     t.integer  "course_id"
-  end
-  
-  create_table "courses_students", :id => false, :force => true do |t|
-    t.integer "student_id", :null => false
-    t.integer "course_id",  :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "students", :force => true do |t|
