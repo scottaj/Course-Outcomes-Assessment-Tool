f158d98ab5ece56f8943f376fbe2afa1b4fd64be
Al Scott <scottaj2@udmercy.edu>
2012-10-31 23:30:12 -0400

    Added moving of Program outcomes.
    Finishes the Program Outcomes portion of #7

--- app/controllers/admin.rb
+++ app/controllers/admin.rb
@@ -23,4 +23,24 @@ CourseOutcomes.controllers :admin do
     
     render "admin/program_outcomes", locals: {page_title: "Program Outcomes Administration", outcomes: program_outcomes}
   end
+
+  get :program_outcomes, with: [:enum, :direction] do
+    move_to = case params[:direction]
+              when /up/i
+                get_previous_outcome(params[:enum])
+              when /down/i
+                get_next_outcome(params[:enum])
+              else
+                nil
+              end
+
+    unless move_to.nil?
+      moving = ProgramOutcome.find_by_enum(params[:enum])
+      moving.outcome, move_to.outcome = move_to.outcome, moving.outcome
+
+      moving.save
+      move_to.save
+    end
+    redirect "admin/program_outcomes"
+  end
 end
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,7 +1,14 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 CourseOutcomes.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+  def get_next_outcome(enum)
+    return ProgramOutcome.find_by_enum(enum.succ)
+  end
+
+  def get_previous_outcome(enum)
+    previous_enum = (enum =~ /^\d+$/ ? 1 : "a")
+    previous_enum.upto(enum) {|i| previous_enum = i if i.succ == enum}
+
+    return ProgramOutcome.find_by_enum(previous_enum)
+  end
 end
