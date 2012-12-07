c0dd8f5ecab3bbf8547d1747b351ba3c06105a89
Al Scott <scottaj2@udmercy.edu>
2012-12-07 14:14:53 -0500

    Added students to seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,10 @@ User.create(username: "lupinr",
             password: "1234abcd",
             first_name: "Remus",
             last_name: "Lupin",
-            active: false,
             role: :user)
+u = User.find_by_username("lupinr")
+u.active = false
+u.save
 
 ### Courses ###
 ## Term 1 2012 ##
@@ -81,4 +83,37 @@ ProgramOutcome.create(outcome: "Write detailed academic reports on magical subje
 ProgramOutcome.create(outcome: "Use magic ethically in everyday life")
 
 
-### Course Outcomes ###
+### Students ###
+Student.create(student_id: "T01234567",
+               first_name: "Harry",
+               last_name: "Potter")
+Student.create(student_id: "T01112223",
+               first_name: "Ronald",
+               last_name: "Weasley")
+Student.create(student_id: "T01231234",
+               first_name: "Hermione",
+               last_name: "Granger")
+Student.create(student_id: "T02929292",
+               first_name: "Nevil",
+               last_name: "Longbottom")
+Student.create(student_id: "T06666666",
+               first_name: "Draco",
+               last_name: "Malfoy")
+Student.create(student_id: "T06767767",
+               first_name: "Cedric",
+               last_name: "Diggory")
+Student.create(student_id: "T03334333",
+               first_name: "Vincent",
+               last_name: "Crabbe")
+Student.create(student_id: "T09879789",
+               first_name: "Gregory",
+               last_name: "Goyle")
+Student.create(student_id: "T04565432",
+               first_name: "Luna",
+               last_name: "Lovegood")
+Student.create(student_id: "T09998887",
+               first_name: "Ginny",
+               last_name: "Weasley")
+Student.create(student_id: "T03458761",
+               first_name: "Cho",
+               last_name: "Chang")
